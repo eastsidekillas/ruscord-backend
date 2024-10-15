9653f2eb5a66d3db7f77648e27a3664aac7b6888
Timofey Tsepennikov <tta@biznpro.ru>
2024-10-16 01:19:27 +0500

    Revert "Add project"
    This reverts commit 1179245a488d2a29778ff4a9f47f36822b7754b3.

--- app_users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'phone', 'avatar_url', 'bio']
