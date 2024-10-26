fc91736cc438fbc0798bdc992d3a550c6ac66043
Timofey Tsepennikov <tta@biznpro.ru>
2024-10-26 20:03:34 +0500

    Add app message and changes friends

---
+++ app_messages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from app_users.models import CustomUser
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='received_messages')
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"From {self.sender} to {self.recipient}: {self.text[:30]}"
