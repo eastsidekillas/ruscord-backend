3e0d8e58412d950774958f57dc6374319ea513ed
Timofey Tsepennikov <tta@biznpro.ru>
2025-01-18 22:50:38 +0500

    Fix search users

--- src/app_users/views.py
+++ src/app_users/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .models import CustomUser
+from django.db import models
+from django.db.models import Q
+from app_friends.models import Friend, FriendRequest
 from .serializers import UserSerializer, RegisterSerializer
 
 
@@ -31,15 +34,34 @@ class UserViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'], url_path='search')
     def search(self, request):
         username = request.query_params.get('username', None)
+        page = request.query_params.get('page', 1)  # Получаем параметр страницы
+        page_size = 10  # Размер страницы, например, 10 пользователей на страницу
 
         if username:
-            # исключаем текущего пользователя из поиска
+            # Исключаем текущего пользователя из поиска
             users = CustomUser.objects.exclude(id=request.user.id).filter(username__icontains=username)
+
+            # Получаем список пользователей, с которыми есть активные заявки (отправленные или полученные)
+            sent_requests = FriendRequest.objects.filter(from_user=request.user).values_list('to_user', flat=True)
+            received_requests = FriendRequest.objects.filter(to_user=request.user).values_list('from_user', flat=True)
+            friends = Friend.objects.filter(
+                models.Q(sender=request.user) | models.Q(receiver=request.user)
+            ).values_list('receiver', flat=True)
+
+            # Исключаем пользователей, с которыми есть заявки или которые уже являются друзьями
+            users = users.exclude(id__in=sent_requests)  # Исключаем тех, кому мы отправили заявку
+            users = users.exclude(id__in=received_requests)  # Исключаем тех, кто нам отправил заявку
+            users = users.exclude(id__in=friends)  # Исключаем уже добавленных друзей
+
+            # Пагинация
+            start = (int(page) - 1) * page_size
+            end = start + page_size
+            users = users[start:end]  # Загружаем пользователей только на текущей странице
+
             serializer = self.get_serializer(users, many=True)
             return Response(serializer.data)
 
         return Response([])
-
 
 class UserRegisterViewSet(APIView):
 
